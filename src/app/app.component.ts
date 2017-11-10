647c6fda6636b7331405259f879346c52c3b3f08
kkneville <mskatherinekrueger@gmail.com>
2017-11-10 10:16:56 -0600

    working on css manipulation with typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,43 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-	const a = ['a','b','c','d','e','f',0,1,2,3,4,5,6,7,8,9];
-	colors = [];
-  	var title = 'Ponyville!';
-	var mail = [
+
+	title = 'Ponyville!';
+	
+	mail = [
 		{ email: "randy@marsh.com", importance: true, subject: "Theeeeeeenks", content: "Creme Fraishe" },
 		{ email: "bryan@griffin.com", importance: true, subject: "My Novel", content: "I like to sniff butts" },
 		{ email: "rick@morty.com", importance: false, subject: "Atlantis", content: "The citadel is watching you" }
-	];
+	];	
 
-	var generate = function(){
-		let color = "#"
-		for (var i = 0; i < 6; i++){
-			color += a[Math.floor(Math.random()*16)]
-		}
-		return color
-	};
-	
-	var list = function(){
+	thing = "red"
+
+	bg = function(){
+		var a = ['a','b','c','d','e','f',0,1,2,3,4,5,6,7,8,9];
+		var colors = []
 		for (var i=0; i< 10; i++){
-			colors.push(generate())
+			var color = "#"
+			for (var b = 0; b < 6; b++){
+				color += a[Math.floor(Math.random()*16)]
+				colors.push(color)
+			}
 		}
 		return colors
-	};
+	}
 
+	// const b = ['AliceBlue', 'Aqua', 'Bisque', 'Black', 'Brown', 'CadetBlue', 'Coral,' 'Cornsilk', 'DarkBlue', 'DarkGray', 'DarkOrchid', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateGray', 'DodgerBlue', 'FireBrick', 'ForestGreen', 'GoldenRod', 'GreenYellow', 'Green', 'Ingigo,' 'LawnGreen', 'LightCyan', 'LightGray', 'Linen', 'MediumPurple', 'MedidumSpringGreen', 'MediumVioletRed', 'Navy', 'OliveDrab', 'PaleTurqoise', 'PaleVioletRed', 'PaleGreen', 'Yellow']
+
+	// var bg = function(){
+	// 	console.log(b[Math.floor(Math.random()*b.length)])
+	// 	return b[Math.floor(Math.random()*b.length)]
+	// }
+
+	// var changeBackground(): any {
+	// 	var color = function(){
+	// 		return b[Math.floor(Math.random()*b.length)]
+	// 	}
+	// 	return { 'background-color': color } 
+	// }
 
 
 }
