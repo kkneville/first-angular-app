92871e2494e9e185fa8ef9aaebc6f15323efeee9
kkneville <mskatherinekrueger@gmail.com>
2017-11-09 20:23:28 -0600

    first angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+	const a = ['a','b','c','d','e','f',0,1,2,3,4,5,6,7,8,9];
+	colors = [];
+  	var title = 'Ponyville!';
+	var mail = [
+		{ email: "randy@marsh.com", importance: true, subject: "Theeeeeeenks", content: "Creme Fraishe" },
+		{ email: "bryan@griffin.com", importance: true, subject: "My Novel", content: "I like to sniff butts" },
+		{ email: "rick@morty.com", importance: false, subject: "Atlantis", content: "The citadel is watching you" }
+	];
+
+	var generate = function(){
+		let color = "#"
+		for (var i = 0; i < 6; i++){
+			color += a[Math.floor(Math.random()*16)]
+		}
+		return color
+	};
+	
+	var list = function(){
+		for (var i=0; i< 10; i++){
+			colors.push(generate())
+		}
+		return colors
+	};
+
+
+
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component
   ],
   imports: [
     BrowserModule
@@ -15,4 +17,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
